1f63f2843053944fc10ee413690dba123def7f4a
Mike Bordelon <374mike@gmail.com>
2016-06-22 12:58:48 -0700

    completed filter, reject and uniq

--- src/underbar.js
+++ src/underbar.js
@@ -77,16 +77,38 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function(item){
+      if(test(item)){
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item){
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var obj = {};
+    var result = [];
+
+    _.each(array, function(item){
+      obj[item] = item;
+    });
+
+    _.each(obj, function(key){
+      result.push(key);
+    });
+    return result;
   };
 
 
